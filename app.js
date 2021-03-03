c54bc530addb41f74920b2008fd8f19d1efc9e35
Arslan <arslanbutt7545@gmail.com>
2021-03-03 10:06:32 +0500

    Add templet

--- app.js
+++ app.js
@@ -3,9 +3,22 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'ejs');
 
 app.get('/',function(req,res){
-  res.send("Yahallo!");
+  var today = new Date();
+  var currentDay = today.getDay();
+
+  var day = "";
+
+  if(currentDay === 0 || currentDay === 6){
+    day = "Free";
+  }else {
+    day = "Working";
+  }
+
+  res.render("list",{typeOfDay:day});
+
 });
 
 
